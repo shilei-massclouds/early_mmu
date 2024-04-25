495e7d83b40dd67bdbd9cff5d80d4eca0e3cd0b5
Shi Lei <shi_lei@massclouds.com>
2024-04-25 20:32:24 +0800

    x86_pc: support it

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -9,7 +9,7 @@ cfg_if::cfg_if! {
 cfg_if::cfg_if! {
     if #[cfg(all(target_arch = "x86_64", platform_family = "x86-pc"))] {
         mod x86_pc;
-        pub use self::x86_pc::*;
+        //pub use self::x86_pc::*;
     } else if #[cfg(all(target_arch = "riscv64", platform_family = "riscv64-qemu-virt"))] {
         mod riscv64_qemu_virt;
         pub use self::riscv64_qemu_virt::*;
